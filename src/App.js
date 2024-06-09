63a71b381d9962a4fa493c3622bb9159f8039c34
Nathanael <naelj15@gmail.com>
2024-06-10 01:38:01 +0700

    Edited app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 // Import components
 import Navigation from "./Navigation/Nav";
